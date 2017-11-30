0f9b162ae468c7c04dbc6c7c84639111f5ab4e48
andreavr <anluu11@gmail.com>
2017-11-30 18:15:47 -0500

    Actualizando pestaña estudents

--- js/app.js
+++ js/app.js
@@ -35,3 +35,29 @@ var chargePage = function() {
 };
 
 chargePage ();
+
+
+/* menu*/
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle();
+
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+
+	});
+
+};
